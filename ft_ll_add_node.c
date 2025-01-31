b46ca1aac8d358baf94de535e9055292cfa358d4
Kristaps Zarins <kzarins@2-C-5.42heilbronn.de>
2025-01-31 13:54:23 +0100

    Added support for void * support.
    The tests include a char * type but the pointer could be anything.

--- ft_ll_add_node.c
+++ ft_ll_add_node.c
@@ -1,81 +1,93 @@
-#include <unistd.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ll_add_node.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzarins <kzarins@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:29:41 by kzarins           #+#    #+#             */
+/*   Updated: 2025/01/31 13:49:18 by kzarins          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ll.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-t_node  *add_node(const int value)
+t_node	*add_node(void *value)
 {
-    t_node *new_head;
+	t_node	*new_head;
 
-    new_head = (t_node *)malloc(sizeof(t_node));
-    if (!new_head)
-        return (NULL);
-    new_head->value = value;
-    new_head->next = NULL;
-    return (new_head);
+	new_head = (t_node *)malloc(sizeof(t_node));
+	if (!new_head)
+		return (NULL);
+	new_head->value = value;
+	new_head->next = NULL;
+	return (new_head);
 }
 
-t_node  *add_ll_back_node(t_node *head, const int value)
+t_node	*add_ll_back_node(t_node *head, void *value)
 {
-    if (!head)
-        return (0);
-    while(head->next)
-    {
-        head = head->next;
-    }
-    head->next = (t_node *)malloc(sizeof(t_node));
-    if (!head->next)
-        return (NULL);
-    head = head->next;
-    head->value = value;
-    head->next = NULL;
-    return (head);
+	if (!head)
+		return (0);
+	while (head->next)
+	{
+		head = head->next;
+	}
+	head->next = (t_node *)malloc(sizeof(t_node));
+	if (!head->next)
+		return (NULL);
+	head = head->next;
+	head->value = value;
+	head->next = NULL;
+	return (head);
 }
 
-t_node  *add_ll_node_after(t_node *previous_node, const int value)
+t_node	*add_ll_node_after(t_node *previous_node, void *value)
 {
-    t_node *new_node;
+	t_node	*new_node;
 
-    new_node = add_node(value);
-    if (!new_node)
-        return (NULL);
-    new_node->next = previous_node->next;
-    previous_node->next = new_node;
-    return (new_node);
+	new_node = add_node(value);
+	if (!new_node)
+		return (NULL);
+	new_node->next = previous_node->next;
+	previous_node->next = new_node;
+	return (new_node);
 }
 
-t_node  *add_ll_node_head(t_node *head, const int value)
+t_node	*add_ll_node_head(t_node *head, void *value)
 {
-    t_node *new_head;
+	t_node	*new_head;
 
-    new_head = add_node(value);
-    if (!new_head)
-        return(head);
-    new_head->next = head;
-    return (new_head);
+	new_head = add_node(value);
+	if (!new_head)
+		return (head);
+	new_head->next = head;
+	return (new_head);
 }
 
-t_node  *add_ll_middle_node(t_node *head, const int value)
+t_node	*add_ll_middle_node(t_node *head, void *value)
 {
-    t_node *fast_ptr;
-    t_node  *previous_ptr;
+	t_node	*fast_ptr;
+	t_node	*previous_ptr;
 
-    fast_ptr = head;
-    if (!head)
-        return (NULL);
-    if (!head->next)
-    {
-        head->next = add_node(value);
-        return (head->next);
-    }
-    while (fast_ptr && fast_ptr->next)
-    {
-        previous_ptr = head;
-        head = head->next;
-        fast_ptr = fast_ptr->next->next;
-    }
-    if (!fast_ptr)
-        head = add_ll_node_after(previous_ptr, value);
-    else
-        head = add_ll_node_after(head, value);
-    return (head);
+	fast_ptr = head;
+	if (!head)
+		return (NULL);
+	if (!head->next)
+	{
+		head->next = add_node(value);
+		return (head->next);
+	}
+	while (fast_ptr && fast_ptr->next)
+	{
+		previous_ptr = head;
+		head = head->next;
+		fast_ptr = fast_ptr->next->next;
+	}
+	if (!fast_ptr)
+		head = add_ll_node_after(previous_ptr, value);
+	else
+		head = add_ll_node_after(head, value);
+	return (head);
 }
---
+++ tests_ft_ll.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests_ft_ll.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzarins <kzarins@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:51:16 by kzarins           #+#    #+#             */
+/*   Updated: 2025/01/31 13:53:38 by kzarins          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ll.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	t_node	*linked_list;
+	t_node	*head;
+
+	linked_list = add_node("ASD");
+	add_ll_back_node(linked_list, "Change");
+	linked_list->next->next = add_node("That");
+	linked_list = add_ll_node_head(linked_list, "Matters");
+	add_ll_middle_node(linked_list, "that");
+	delete_ll_middle_node(linked_list);
+	delete_ll_last_node(linked_list);
+	linked_list = delete_ll_node_head(linked_list);
+	head = linked_list;
+	while (linked_list)
+	{
+		printf("%s\n", (char *)linked_list->value);
+		linked_list = linked_list->next;
+	}
+	delete_ll_all(&head);
+	while (head)
+	{
+		head = head->next;
+		printf("hello");
+	}
+	return (EXIT_SUCCESS);
+}
